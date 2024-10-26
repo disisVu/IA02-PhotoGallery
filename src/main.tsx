e83f3aa861feabdd7ce78f7165ca3f4f9f492fdf
disisVu <102909483+disisVu@users.noreply.github.com>
2024-10-26 14:50:09 +0700

    Path update

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename='/IA02-PhotoGallery'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
