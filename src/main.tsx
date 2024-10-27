323e3fe5da812cd1bfc2fa422ac011dc43892fed
disisVu <102909483+disisVu@users.noreply.github.com>
2024-10-27 10:09:46 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter basename='/IA02-PhotoGallery'>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
+  // <React.StrictMode>
+  // </React.StrictMode>
 )
